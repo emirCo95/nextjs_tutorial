ab088425616bc3d1b2027f813958d5cdce954535
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-02-03 16:09:31 -0500

    navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import Navigation from './components/Navigation';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,9 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <header className="bg-slate-900 text-white p-4 text-center">
+        {/* <header className="bg-slate-900 text-white p-4 text-center">
           <p>Welcome to Next.js 15 Crash Course</p>
-        </header>
+        </header> */}
+        <Navigation />
         {children}
         <footer className="bg-slate-900 text-white p-4 text-center">
           Codevolution
